45d8ac293180a6bd552f63c54c68dd14c0ab5259
rmatta <ramdharam@gmail.com>
2018-02-15 23:41:11 -0700

    adding .gitignore files

--- homeTimeline.py
+++ homeTimeline.py
@@ -1,11 +1,12 @@
 import tweepy
 import json
 import os
+import sys
 
 cwd = os.getcwd()
-print (cwd)
+#print (cwd)
 file_path = os.path.join(cwd, 'creds.json')
-print (file_path)
+#print (file_path)
 
 
 with open(file_path) as json_data:
@@ -15,6 +16,11 @@ consumer_key=d['consumer_key']
 consumer_secret=d['consumer_secret']
 access_token=d['access_token']
 access_token_secret=d['access_token_secret']
-print(consumer_key)
 
-
+auth=tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    #print(tweet)
+    sys.stdout.write(tweet.text)
---
+++ streamingAPI.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+from tweepy import OAuthHandler, StreamListener, Stream
+import json
+import sys
+import os
+from kafka import KafkaClient, KafkaProducer, SimpleClient
+
+filepath=os.path.join("creds.json")
+with open(filepath,'r') as json_data:
+    creds = json.load(json_data)
+
+consumer_key=creds['consumer_key']
+consumer_secret=creds['consumer_secret']
+access_token=creds['access_token']
+access_token_secret=creds['access_token_secret']
+
+auth=OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+class StdOutListener(StreamListener):
+    def on_data(self, raw_data):
+        #producer.send("taylorsville", raw_data.encode('utf-8'))
+        #print (raw_data)
+        producer.send(topic="taylorsville", key=b'foo')
+    def on_error(self, status_code):
+        print status_code
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+l = StdOutListener()
+stream = Stream(auth,l)
+stream.filter(track="taylorsville", encoding='utf-8')
+
+
